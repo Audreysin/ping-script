0e5361f9804bac8d910ece41e59fd5303e620d35
Audrey Sin Fai Lam <audreysinfailam@Audreys-MacBook-Air.local>
2019-09-05 21:57:21 -0400

    Adding script

---
+++ ping script.ps1
@@ -0,0 +1,20 @@
+﻿
+# Requires the input file to have a column header named 'Pingable?' which is to be updated by the script
+
+# Set $path to be the path of the file containing the servers/ip address... to be pinged
+$path = "C:\Users\aulam\Documents\vi report\vi server list.csv"
+$list = import-csv $path
+
+Write-Host "Started Pinging.."
+
+foreach($item in $list) {
+    if (test-connection $ip.("Name"<#the name of the column header whose values a re to be pinged#>) -count 1 -quiet) {
+        $list.'Pingable?' = 'Yes'
+
+    } else {
+         $list.'Pingable?' = 'No'
+    }
+    [pscustomobject] $list | export-csv -path $path -NoTypeInformation
+}
+
+Write-Host "Ping complete"
